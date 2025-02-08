dfeb357e91e5d6116140ec2524694d586491ad13
Allyson Mitchem <allysonmitchem@gmail.com>
2025-02-07 21:16:50 -0700

    added data types

--- ConsoleApp/giraffe/Program.cs
+++ ConsoleApp/giraffe/Program.cs
@@ -24,29 +24,50 @@
 
 // ---VARIABLES---
 
+// namespace giraffe;
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         //creating variables
+//         string characterName = "John";
+//         int characterAge = 35;
+
+//         //or can declare variable first 
+//         // int characterAge;
+//         // characterAge = 35;
+
+//         Console.WriteLine("There once was a man named " + characterName);
+//         Console.WriteLine("He was " + characterAge + " years old");
+        
+//         //updating value halfway through
+//         characterName = "Mike";
+
+//         Console.WriteLine("He really liked the name " + characterName);
+//         Console.WriteLine("But didn't like being " + characterAge);
+
+//         Console.ReadLine();
+
+//     }
+// }
+
+
+//--- DATA TYPES ---
+
 namespace giraffe;
 class Program
 {
     static void Main(string[] args)
     {
-        //creating variables
-        string characterName = "John";
-        int characterAge = 35;
-
-        //or can declare variable first 
-        // int characterAge;
-        // characterAge = 35;
-
-        Console.WriteLine("There once was a man named " + characterName);
-        Console.WriteLine("He was " + characterAge + " years old");
+        string phrase = "Giraffe Academy";
+        //for characters, can only use single character
+        char grade = 'A';
+        int age = 30; //can be whole or negative numbers, as well as decimals
+        float, double, decimal //least accurate to most accurate with decimals
+        double gpa = 3.2;
+         // boolean => [true or false value]
+        bool isMale = true; 
         
-        //updating value halfway through
-        characterName = "Mike";
-
-        Console.WriteLine("He really liked the name " + characterName);
-        Console.WriteLine("But didn't like being " + characterAge);
-
         Console.ReadLine();
-
     }
 }
